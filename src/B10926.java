3a2332e749f7813d9a72b198cdd5da30fd224729
gibum1228 <gibum1228@naver.com>
2020-09-07 21:55:42 +0900

    ??!

---
+++ src/B10926.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class B10926 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String str = sc.nextLine();
+		
+		System.out.println(str + "??!");
+		
+		sc.close();
+	}
+
+}
